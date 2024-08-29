a917e2e8ef88122a396ff59d31053e6ff899c10e
William Garcia <wgarcia@tradercomusa.com>
2024-08-29 11:14:29 -0400

    feats: Add images and confiure of external images.

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,57 @@ const nextConfig = {
           hostname: 'pilatos21.vteximg.com.br',
           port: '',
           pathname: '*/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'www.malishop.cl',
+          port: '',
+          pathname: '*/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'cdn.shopify.com',
+          port: '',
+          pathname: '*/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'api-prd.ynk.cl',
+          port: '',
+          pathname: '*/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'bosico.vteximg.com.br',
+          port: '',
+          pathname: '*/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'thenorthfaceco.vteximg.com.br',
+          port: '',
+          pathname: '*/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'thenorthfaceco.vteximg.com.br',
+          port: '',
+          pathname: '*/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'superdrycolombia.vteximg.com.br',
+          port: '',
+          pathname: '*/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'dieselcolombia.vteximg.com.br',
+          port: '',
+          pathname: '*/**',
         }
+        
+        //superdrycolombia.vteximg.com.br
       ],
     },
   };
