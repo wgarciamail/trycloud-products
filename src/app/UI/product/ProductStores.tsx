a4d3b4c4f45b3e4e0e93e9aec8b516c89df67e86
William Garcia <wgarcia@tradercomusa.com>
2024-09-04 16:32:05 -0400

    feats: Add information to product Store component

--- src/app/UI/product/ProductStores.tsx
+++ src/app/UI/product/ProductStores.tsx
@@ -1,23 +1,37 @@
-import { fetchProductByTN } from "@/app/lib/data";
+import { fetchCustomerData, fetchProductByTN } from "@/app/lib/data";
 import React from "react";
 
 
 const ProductStores = async({TN}: { TN: string | null}) => {
   if (TN == null) return <p>Error: falta el parámetro TN</p>
-  const data = await fetchProductByTN(TN)
-  if (data == null || data.stores == null || data.stores.length == 0) return <p>No hay existencias/tiendas</p>
-  console.log(data)
-  const {stores} = data
+  const dataProduct = await fetchProductByTN(TN)
+  const dataCloseStore = await fetchCustomerData();
+  console.log(dataCloseStore.stores);
+  const {stores} = dataProduct
   return (
-    <>
-        <h2 className="text-lg font-bold">Stores</h2>
-        <ul className="list-disc pl-5">
-            { stores.map((store: any) => (
-                <li key={store.warehouse}>{store.warehouse}- ({store.quantity})</li>
+    <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+      <div className="col-span-1">
+          <h2 className="text-lg font-bold">Tiendas del producto</h2>
+          {dataProduct == null || dataProduct.stores == null || dataProduct.stores.length == 0 ? <p>No hay existencias/tiendas</p> :
+          (<ul className="list-disc pl-5 border">
+              { stores.map((store: any) => (
+                  <li key={store.warehouse}>{store.warehouse}- ({store.quantity})</li>
+              ))}
+          </ul>)
+          }
+      </div>
+      <div className="col-span-2">
+        <h2 className="text-lg font-bold">Tiendas Cercanas</h2>
+        {dataCloseStore == null || dataCloseStore.stores == null || dataCloseStore.stores.length == 0 ? <p>No hay tiendas</p> :  
+        (<ul className="list-disc pl-5 border">
+            { dataCloseStore.stores.map((store: any) => (
+                <li key={store.store_id}>{store.store_code}- ({store.name}) - {store.distance.toFixed(2)}</li>
             ))}
-           
         </ul>
-    </>
+        )}
+      </div>
+    </div>
+
   );
 };
 
