52a02e2f3577058cc49121df5100be84696f3907
William Garcia <wgarcia@tradercomusa.com>
2024-09-04 17:53:57 -0400

    feats: Show size selected by the client.

--- src/app/UI/product/ProductSize.tsx
+++ src/app/UI/product/ProductSize.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import React from "react";
-//import { useRouter } from "next/router";
+import clsx from "clsx";
 import { useRouter } from 'next/navigation';
+import { useSearchParams } from 'next/navigation'
 
 
 const ProductSize = ({ variationsColor, TNP}: { variationsColor: any, TNP: string }) => {
   const router = useRouter();
+  const searchParams = useSearchParams()
+  const sizeName = searchParams.get('sizeName')
   const selectSize = async (tnp: string, tn: string, sizeName: string) => {
      router.push(`/dashboard/products/${tnp}?tn=${tn}&sizeName=${sizeName}`)
   }
@@ -18,7 +21,10 @@ const ProductSize = ({ variationsColor, TNP}: { variationsColor: any, TNP: strin
         <select className="border p-2 w-full">
           <option>Selecciona la talla...</option>
           {variationsColor[0].variationsSize.map((variation: any) => (
-            <option key={variation.sizeName} value="0">
+            <option 
+              key={variation.sizeName}
+              value="0"
+              >
               {variation.sizeName}
             </option>
           ))}
@@ -26,7 +32,13 @@ const ProductSize = ({ variationsColor, TNP}: { variationsColor: any, TNP: strin
       </div>
       <div className="hidden md:flex flex-wrap">
         {variationsColor[0].variationsSize.map((variation: any) => (
-          <button key={variation.TN} className="border rounded p-2 w-10 h-10 m-1" onClick={() => selectSize(TNP, variation.TN, variation.sizeName)}>{variation.sizeName}</button>
+          <button 
+          key={variation.TN} 
+          className={clsx(
+            "border rounded p-2 w-10 h-10 m-1",
+            { "bg-violet-600 text-white" : variation.sizeName === sizeName },
+        )}
+          onClick={() => selectSize(TNP, variation.TN, variation.sizeName)}>{variation.sizeName}</button>
         ))}
       </div>
     </>
