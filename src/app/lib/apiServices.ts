b8e8504ae4a3e3111eb6e4c272f99f42cb12126d
William Garcia <wgarcia@tradercomusa.com>
2024-10-21 11:31:31 -0400

    feats: change baseUrl to api in production

--- src/app/lib/apiServices.ts
+++ src/app/lib/apiServices.ts
@@ -3,8 +3,8 @@ import { AuthService } from './authService';
 
 // Create an instance of axios
 const api: AxiosInstance = axios.create({
-    baseURL: 'http://trycloud-api.traetelo.me:8091',
-    //baseURL: 'https://api.trycloud.io',
+    //baseURL: 'http://trycloud-api.traetelo.me:8091',
+    baseURL: 'https://api.trycloud.io',
     //baseURL: process.env.NEXT_PUBLIC_API_URL,
     withCredentials: true,
     headers: {
